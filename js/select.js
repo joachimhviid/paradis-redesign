92807d00494d257ed4db2e6f4bd12c49fec30d5b
jokkejok123 <47396357+jokkejok123@users.noreply.github.com>
2019-12-10 15:30:45 +0100

    Made select only toggle 1 box

--- js/select.js
+++ js/select.js
@@ -1,12 +1,19 @@
-function Select(button) {
+var section = document.getElementById("butik");
+var cards = section.getElementsByClassName("main-button");
+
+Select = (button) => {
     var checkbox = button.parentNode.querySelector("input");
     if (checkbox.checked === true) {
         checkbox.checked = false;
-        button.classList.toggle("active");
+        button.classList.remove("active");
         button.innerHTML = "Vælg";
     } else {
         checkbox.checked = true;
-        button.classList.toggle("active");
+        for (let i = 0; i < cards.length; i++) {
+            cards[i].classList.remove("active");
+            cards[i].innerHTML = "Vælg";
+        }
+        button.classList.add("active");
         button.innerHTML = "Valgt";
     }
 }
