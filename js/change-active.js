010deb9d4c95788e5f92de5fcc62afcd24ca4572
jokkejok123 <47396357+jokkejok123@users.noreply.github.com>
2019-12-10 14:18:22 +0100

    Removed redundant section from config.html. Adjusted JS accordingly

--- js/change-active.js
+++ js/change-active.js
@@ -6,7 +6,6 @@ var menuBtns = configMenu.getElementsByClassName("configmenubutton");
 
 // Array of all the section IDs
 var configSections = [
-    document.getElementById("butik"),
     document.getElementById("grundsmage"),
     document.getElementById("ekstraSmage"),
     document.getElementById("striber"),
@@ -15,12 +14,10 @@ var configSections = [
 ];
 
 var configText = document.getElementById("headConfigText");
-var icecreamElement = document.getElementById("icecream");
-var searchElement = document.getElementById("butiksearch");
 
 // Loop through the buttons and add the active class to the current/clicked button
 for (var i = 0; i < menuBtns.length; i++) {
-    menuBtns[i].addEventListener("click", function() {
+    menuBtns[i].addEventListener("click", function () {
         let currentMenu = document.getElementsByClassName(" active");
         currentMenu[0].className = currentMenu[0].className.replace(" active", "");
         this.className += " active";
@@ -31,14 +28,6 @@ ChangeSection = (id) => {
     let current = document.getElementsByClassName(" active-section");
     current[0].className = current[0].className.replace(" active-section", "");
     id.className += " active-section";
-    if (id == butik) {
-        icecreamElement.style.display = "none";
-        searchElement.style.display = "block";
-        configText.innerHTML = "Vælg den butik du vil hente Din Paradis hos.";
-    } else {
-        icecreamElement.style.display = "block";
-        searchElement.style.display = "none";
-    }
     id == grundsmage ? configText.innerHTML = "Vælg en grundsmag til Din Paradis." : null;
     id == ekstraSmage ? configText.innerHTML = "Vælg ekstra smage til Din Paradis. (Valgfrit)" : null;
     id == striber ? configText.innerHTML = "Vælg striber til Din Paradis. (Valgfrit)" : null;
@@ -55,12 +44,6 @@ NextSection = () => {
         if (currentID == configSections[configSections.length - 1].id) {
             return;
         }
-        // Checks if the current active section is the first and shows relevant top element accordingly for next section
-        if (currentID == configSections[0].id) {
-            icecreamElement.style.display = "block";
-            searchElement.style.display = "none";
-            configText.innerHTML = "Vælg en grundsmag til Din Paradis.";
-        }
         if (currentID == configSections[i].id) {
             configSections[i].className = configSections[i].className.replace(" active-section", "");
             currentMenu[0].className = currentMenu[0].className.replace(" active", "");
@@ -68,10 +51,10 @@ NextSection = () => {
             configSections[i].className += " active-section";
             menuBtns[i].className += " active";
         }
-        currentID == configSections[1].id ? configText.innerHTML = "Vælg ekstra smage til Din Paradis. (Valgfrit)" : null;
-        currentID == configSections[2].id ? configText.innerHTML = "Vælg striber til Din Paradis. (Valgfrit)" : null;
-        currentID == configSections[3].id ? configText.innerHTML = "Vælg knasende stykker til Din Paradis. (Valgfrit)" : null;
-        currentID == configSections[4].id ? configText.innerHTML = "Bekræft dine valg og føj Din Paradis til din kurv." : null;
+        currentID == configSections[0].id ? configText.innerHTML = "Vælg ekstra smage til Din Paradis. (Valgfrit)" : null;
+        currentID == configSections[1].id ? configText.innerHTML = "Vælg striber til Din Paradis. (Valgfrit)" : null;
+        currentID == configSections[2].id ? configText.innerHTML = "Vælg knasende stykker til Din Paradis. (Valgfrit)" : null;
+        currentID == configSections[3].id ? configText.innerHTML = "Bekræft dine valg og føj Din Paradis til din kurv." : null;
     }
 };
 
@@ -84,12 +67,6 @@ PrevSection = () => {
         if (currentID == configSections[0].id) {
             return;
         }
-        // Checks if the current active section is going to be the first and shows relevant top element accordingly
-        if (currentID == configSections[1].id) {
-            icecreamElement.style.display = "none";
-            searchElement.style.display = "block";
-            configText.innerHTML = "Vælg den butik du vil hente Din Paradis hos.";
-        }
         if (currentID == configSections[i].id) {
             configSections[i].className = configSections[i].className.replace(" active-section", "");
             currentMenu[0].className = currentMenu[0].className.replace(" active", "");
@@ -98,9 +75,9 @@ PrevSection = () => {
             configSections[e].className += " active-section";
             menuBtns[e].className += " active";
         }
-        currentID == configSections[2].id ? configText.innerHTML = "Vælg en grundsmag til Din Paradis." : null;
-        currentID == configSections[3].id ? configText.innerHTML = "Vælg ekstra smage til Din Paradis. (Valgfrit)" : null;
-        currentID == configSections[4].id ? configText.innerHTML = "Vælg striber til Din Paradis. (Valgfrit)" : null;
-        currentID == configSections[5].id ? configText.innerHTML = "Vælg knasende stykker til Din Paradis. (Valgfrit)" : null;
+        currentID == configSections[1].id ? configText.innerHTML = "Vælg en grundsmag til Din Paradis." : null;
+        currentID == configSections[2].id ? configText.innerHTML = "Vælg ekstra smage til Din Paradis. (Valgfrit)" : null;
+        currentID == configSections[3].id ? configText.innerHTML = "Vælg striber til Din Paradis. (Valgfrit)" : null;
+        currentID == configSections[4].id ? configText.innerHTML = "Vælg knasende stykker til Din Paradis. (Valgfrit)" : null;
     }
 }
